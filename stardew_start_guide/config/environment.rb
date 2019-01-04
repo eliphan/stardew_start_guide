8a3e8294dc557ffcabcabee34aa83d7a0c3e9e2f
eliphan <aliceconnic@gmail.com>
2019-01-04 00:48:59 -0800

    Delete environment.rb

--- stardew_start_guide/config/environment.rb
+++
@@ -1,8 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-require_relative '../lib/stardew_start_guide/scraper'
-require_relative '../lib/stardew_start_guide/guide'
-require_relative '../lib/stardew_start_guide/cli'
-require_relative '../lib/stardew_start_guide/version'
