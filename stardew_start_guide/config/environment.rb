a279830c1149d2ea084b12019553f9a1ebeba6a4
eliphan <aliceconnic@gmail.com>
2019-01-03 22:33:13 -0800

    Add files via upload

---
+++ stardew_start_guide/config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/stardew_start_guide/scraper'
+require_relative '../lib/stardew_start_guide/guide'
+require_relative '../lib/stardew_start_guide/cli'
+require_relative '../lib/stardew_start_guide/version'
